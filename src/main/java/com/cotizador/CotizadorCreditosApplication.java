f4fe427c5caee767fe1ec42d05099af8c34585a9
funerhk <funerhk@gmail.com>
2024-05-21 00:31:00 -0600

    Initial commit

---
+++ src/main/java/com/cotizador/CotizadorCreditosApplication.java
@@ -0,0 +1,13 @@
+package com.cotizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotizadorCreditosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotizadorCreditosApplication.class, args);
+	}
+
+}
